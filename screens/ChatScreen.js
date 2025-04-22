c72c1ee6a2b5a08cc880806355a9ebc0dbc664b6
WinCodeX <glenwinterg970@gmail.com>
2025-04-22 22:52:05 +0300

    Mobile app

---
+++ screens/ChatScreen.js
@@ -0,0 +1,8 @@
+import { View, Text } from 'react-native';
+export default function ChatScreen() {
+  return (
+    <View className="flex-1 bg-dracula-background justify-center items-center">
+      <Text className="text-dracula-foreground text-xl">Chat Screen</Text>
+    </View>
+  );
+}
